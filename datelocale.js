8e0082a21930218d78268dedbd3cd8d4e2bbfef4
fagnerkof <31597186+fagnerkof@users.noreply.github.com>
2020-09-14 18:41:28 -0300

    Add files via upload

---
+++ datelocale.js
@@ -0,0 +1,37 @@
+$(function() {
+        $('input[name="daterange"]').daterangepicker({
+    "locale": {
+        "format": "DD/MM/YYYY",
+        "separator": " - ",
+        "applyLabel": "Apply",
+        "cancelLabel": "Cancel",
+        "fromLabel": "From",
+        "toLabel": "To",
+        "customRangeLabel": "Custom",
+        "daysOfWeek": [
+            "Dom",
+            "Seg",
+            "Ter",
+            "Qua",
+            "Qui",
+            "Sex",
+            "Sab"
+        ],
+        "monthNames": [
+            "Janeiro",
+            "Fevereiro",
+            "Março",
+            "Abril",
+            "Maio",
+            "Junho",
+            "Julho",
+            "Agosto",
+            "Setembro",
+            "Outubro",
+            "Novembro",
+            "Dezembro"
+        ],
+        "firstDay": 1
+    }
+});
+        });
